8e83d4b7669c3e9e94dc3e438e2c2c3f1b0bb6cf
koturn <jeak.koutan.apple@gmail.com>
2018-01-02 20:07:49 +0900

    Fix escape string

--- .common_profile.sh
+++ .common_profile.sh
@@ -1,22 +1,22 @@
 export LANG=ja_JP.UTF-8
-case ${UID} in
+case $UID in
   0)
     LANG=C
     ;;
 esac
 
-export LESS_TERMCAP_mb="\e[1;31m"
-export LESS_TERMCAP_md="\e[1;32m"
-export LESS_TERMCAP_me="\e[0m"
-export LESS_TERMCAP_se="\e[0m"
-export LESS_TERMCAP_so="\e[1;44;33m"
-export LESS_TERMCAP_ue="\e[0m"
-export LESS_TERMCAP_us="\e[4;36m"
+export LESS_TERMCAP_mb=$'\e[1;31m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[1;44;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[4;36m'
 
 export VISUAL=vim
 export EDITOR=vim
 
-case ${OSTYPE} in
+case $OSTYPE in
   cygwin | msys)
     alias clear='echo -ne "\ec\e[3J"'
     export PATH=$PATH:/c/alias/
@@ -24,5 +24,3 @@ case ${OSTYPE} in
     export CHERE_INVOKING=1
     ;;
 esac
-
-
